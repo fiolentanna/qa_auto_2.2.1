009948901016b35cdc7c402a33a126fc9f2f9d10
fiolentanna <tigranka@gmail.com>
2024-09-03 15:34:35 +0300

    2.2.4 added functions for decrement and increment

--- calculations.js
+++ calculations.js
@@ -2,22 +2,51 @@ function increaseBalance(x, y) {
   x += y;
   return x;
 }
-console.log(increaseBalance(3000, 700));
+
 
 function decreaseBalance(x, y) {
 x -= y;
 return x;
 }
-console.log(decreaseBalance(3000, 700));
+
 
 function divideBalanceByAccounts(x, y) {
   x /= y;
   return x;
 }
-console.log(divideBalanceByAccounts(3000, 2));
+
 
 function getRestAfterDivision(x, y) {
   x %= y;
   return x;
 }
+
+
+function getIncrementPost(x) {
+  return x++;
+}
+
+
+function getIncrementPre(x) {
+  return ++x;
+}
+
+
+function getDecrementPost(x) {
+  return x--;
+}
+
+
+function getDecrementPre(x) {
+  return --x;
+}
+
+
+console.log(increaseBalance(3000, 700));
+console.log(decreaseBalance(3000, 700));
+console.log(divideBalanceByAccounts(3000, 2));
 console.log(getRestAfterDivision(7000, 3));
+console.log(getIncrementPost(7));
+console.log(getIncrementPre(6));
+console.log(getDecrementPost(5));
+console.log(getDecrementPre(1));
