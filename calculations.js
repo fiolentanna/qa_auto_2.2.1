f678f67c19bd721d7b4aafb9ef262360ca0467b9
fiolentanna <tigranka@gmail.com>
2024-09-03 12:31:59 +0300

    2.2.1 added changes to calculations.js

--- calculations.js
+++ calculations.js
@@ -2,24 +2,22 @@ function increaseBalance(x, y) {
   x = x + y;
   return x;
 }
+console.log(increaseBalance(3000, 700));
 
-function decreaseBalance(x) {
+function decreaseBalance(x, y) {
 x = x - y;
 return x;
 }
+console.log(decreaseBalance(3000, 700));
 
 function divideBalanceByAccounts(x, y) {
   x = x / y;
   return x;
-  console.log("x");
 }
+console.log(divideBalanceByAccounts(3000, 2));
 
 function getRestAfterDivision(x, y) {
   x = x % y;
   return x;
 }
-
-console.log(increaseBalance(3000, 700));
-console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
 console.log(getRestAfterDivision(7000, 3));
