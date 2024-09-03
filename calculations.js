91ae64ea87408159847dbd6b578e070448e506a1
fiolentanna <tigranka@gmail.com>
2024-09-03 13:15:53 +0300

    2.2.2 added assignment operators to calculations.js

--- calculations.js
+++ calculations.js
@@ -1,23 +1,23 @@
 function increaseBalance(x, y) {
-  x = x + y;
+  x += y;
   return x;
 }
 console.log(increaseBalance(3000, 700));
 
 function decreaseBalance(x, y) {
-x = x - y;
+x -= y;
 return x;
 }
 console.log(decreaseBalance(3000, 700));
 
 function divideBalanceByAccounts(x, y) {
-  x = x / y;
+  x /= y;
   return x;
 }
 console.log(divideBalanceByAccounts(3000, 2));
 
 function getRestAfterDivision(x, y) {
-  x = x % y;
+  x %= y;
   return x;
 }
 console.log(getRestAfterDivision(7000, 3));
